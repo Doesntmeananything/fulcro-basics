192758031f3faee263156605ce402efa4bc14f3b
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-12-10 01:15:19 +0300

    Add initial state and mutation

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -3,22 +3,37 @@
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]))
 
 (defsc Car [this {:car/keys [id model] :as props}]
-  {:query [:car/id :car/model]
-   :ident :car/id}
+  {:query         [:car/id :car/model]
+   :ident         :car/id
+   :initial-state (fn [{:keys [id model]}]
+                    {:car/id    id
+                     :car/model model})}
   (dom/div
     "Model: " model))
 
 (def ui-car (comp/factory Car {:keyfn :car/id}))
 
+(defmutation make-older [{:person/keys [id]}]
+  (action [{:keys [state]}]
+          (swap! state update-in [:person/id id :person/age] inc)))
+
 (defsc Person [this {:person/keys [id name cars age] :as props}]
-  {:query [:person/id :person/name :person/age {:person/cars (comp/get-query Car)}]
-   :ident :person/id}
+  {:query         [:person/id :person/name :person/age {:person/cars (comp/get-query Car)}]
+   :ident         :person/id
+   :initial-state {:person/id   :param/id
+                   :person/name :param/name
+                   :person/age  20
+                   :person/cars [{:id 40 :model "Leaf"}
+                                 {:id 41 :model "Escort"}
+                                 {:id 42 :model "Sienna"}]}}
   (dom/div
     (dom/div "Name: " name)
     (dom/div "Age: " age)
+    (dom/button {:onClick #(comp/transact! this [(make-older {:person/id id})])} "Make Older")
     (dom/h3 "Cars")
     (dom/ul
       (map ui-car cars))))
@@ -28,9 +43,11 @@
 (defonce app (app/fulcro-app))
 
 (defsc Root [this {:root/keys [person]}]
-  {:query [{:root/person (comp/get-query Person)}]}
+  {:query         [{:root/person (comp/get-query Person)}]
+   :initial-state {:root/person {:id 1 :name "Bob"}}}
   (dom/div
-    (ui-person person)))
+    (when person
+      (ui-person person))))
 
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
@@ -45,12 +62,4 @@
   (app/mount! app Root "app"))
 
 (comment
-  (reset! (::app/state-atom app) {})
-
-  (swap! (::app/state-atom app) update-in [:person/id 3 :person/age] inc)
-
-  (merge/merge-component! app Car {:car/id    42
-                                   :car/model "F-150"}
-                          :append [:person/id 3 :person/cars])
-  (app/current-state app)
-  (app/schedule-render! app))
+  (comp/get-initial-state Root))
