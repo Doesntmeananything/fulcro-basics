5c3b987a43323f14270aba3b044ea954d7d1f7e4
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-12-10 20:13:56 +0300

    Add React component composition, interop

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -1,19 +1,22 @@
 (ns app.client
   (:require
+    ["react-number-format" :as NumberFormat]
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-    [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.dom :as dom :refer [div ul li h3 button label]]
+    [com.fulcrologic.fulcro.algorithms.react-interop :as interop]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]))
 
+(def ui-number-format (interop/react-factory NumberFormat))
+
 (defsc Car [this {:car/keys [id model] :as props}]
-  {:query         [:car/id :car/model]
-   :ident         :car/id
-   :initial-state (fn [{:keys [id model]}]
-                    {:car/id    id
-                     :car/model model})}
-  (dom/div
-    "Model: " model))
+  {:query [:car/id :car/model]
+   :ident :car/id
+   :initial-state {:car/id :param/id
+                   :car/model :param/model}}
+  (div :.ui.item
+       "Model: " model))
 
 (def ui-car (comp/factory Car {:keyfn :car/id}))
 
@@ -22,35 +25,56 @@
           (swap! state update-in [:person/id id :person/age] inc)))
 
 (defsc Person [this {:person/keys [id name cars age] :as props}]
-  {:query         [:person/id :person/name :person/age {:person/cars (comp/get-query Car)}]
-   :ident         :person/id
-   :initial-state {:person/id   :param/id
+  {:query [:person/id :person/name :person/age {:person/cars (comp/get-query Car)}]
+   :ident :person/id
+   :initial-state {:person/id :param/id
                    :person/name :param/name
-                   :person/age  20
+                   :person/age 20
                    :person/cars [{:id 40 :model "Leaf"}
                                  {:id 41 :model "Escort"}
                                  {:id 42 :model "Sienna"}]}}
-  (dom/div
-    (dom/div "Name: " name)
-    (dom/div "Age: " age)
-    (dom/button {:onClick #(comp/transact! this [(make-older {:person/id id})])} "Make Older")
-    (dom/h3 "Cars")
-    (dom/ul
-      (map ui-car cars))))
+  (div :.ui.segment
+       (div :.ui.form
+            (div :.field
+                 (label "Name: ")
+                 name)
+            (div :.field
+                 (label "Amount: ")
+                 (ui-number-format {:thousandSeparator true
+                                    :prefix "$"}))
+            (div :.field
+                 (label "Age: ")
+                 age)
+            (button :.ui.button {:onClick #(comp/transact! this [(make-older {:person/id id})])} "Make Older")
+            (h3 "Cars")
+            (div :.ui.list
+                 (map ui-car cars)))))
 
 (def ui-person (comp/factory Person {:keyfn :person/id}))
 
+(defsc PersonList [this {:person-list/keys [people] :as props}]
+  {:query [{:person-list/people (comp/get-query Person)}]
+   :ident (fn [_ _] [:component/id ::person-list])
+   :initial-state {:person-list/people [{:id 1 :name "Bob"}
+                                        {:id 2 :name "Sally"}]}}
+  (div
+    (h3 :.ui.header "People")
+    (map ui-person people)))
+
+(def ui-person-list (comp/factory PersonList))
+
 (defonce app (app/fulcro-app))
 
-(defsc Root [this {:root/keys [person]}]
-  {:query         [{:root/person (comp/get-query Person)}]
-   :initial-state {:root/person {:id 1 :name "Bob"}}}
-  (dom/div
-    (when person
-      (ui-person person))))
+(defsc Root [this {:root/keys [people]}]
+  {:query [{:root/people (comp/get-query PersonList)}]
+   :initial-state {:root/people {}}}
+  (div
+    (when people
+      (ui-person-list people))))
 
 (defn ^:export init
-  "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
+  "Shadow-cljs sets this up to be our entry-point function.
+  See shadow-cljs.edn `:init-fn` in the modules of the main build."
   []
   (app/mount! app Root "app")
   (js/console.log "Loaded"))
